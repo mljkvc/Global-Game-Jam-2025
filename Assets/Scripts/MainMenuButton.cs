22985d4752dcbbb2abc1ded8a736d604c5dce6ff
mljkvc <moljkovic5@gmail.com>
2025-01-26 09:35:31 +0100

    Implemented mainmenu

---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+}
